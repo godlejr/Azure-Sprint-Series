70954f7aa6741c3dc213d31a1f7da2d3272b2fc2
dongjoo-kim <godlejr@gmail.com>
2022-07-12 20:55:25 +0900

    final challenge

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM php:7.4-apache
+COPY v1/index.php /var/www/html/
---
+++ v1/index.php
@@ -0,0 +1,14 @@
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<div>
+
+<div class="container">
+<div>안녕 2</div>
+</div>
+
+</html>
